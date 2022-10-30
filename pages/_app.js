2a39d3d1945a9d4206af471bb4057e311b06eeb1
Dedeepya Madineni <dedeepya01@yahoo.com>
2022-10-30 18:38:03 -0400

    most changes requested have been done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css';
 // import { UserProvider } from '@auth0/nextjs-auth0';
-import Upload from './components/volunteer/Upload.jsx';
+import Login from './components/volunteer/Login.jsx';
 import Registration from './components/volunteer/Registration';
+import ProfilePage from './components/volunteer/ProfilePage';
+
 
 function MyApp({ Component, pageProps }) {
 
@@ -9,7 +11,7 @@ function MyApp({ Component, pageProps }) {
   return (
     // <UserProvider>
       <div className="h-screen flex flex-col justify-items-center ">
-        <Upload/>
+        <Registration />
       </div>
     // </UserProvider>
   )
