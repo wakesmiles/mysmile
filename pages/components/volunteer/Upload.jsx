e1f3cba7dde52a1e8f23ac6bd5dcde4a9c24fcee
Dedeepya Madineni <dedeepya01@yahoo.com>
2022-10-30 09:20:45 -0400

    added space before browse

--- pages/components/volunteer/Upload.jsx
+++ pages/components/volunteer/Upload.jsx
@@ -26,7 +26,7 @@ const Upload = () => {
                     d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
               </svg>
               <span class="font-medium text-gray-600">
-                Drop files to Attach, or
+                Drop files to Attach, or 
                 <span class="text-blue-600 underline">browse</span>
               </span>
             </span>
