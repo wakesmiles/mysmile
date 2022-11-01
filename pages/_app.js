4c39e451b764269fb9bb0708c31d58a498b0c447
yshaggy <69633915+yshaggy@users.noreply.github.com>
2022-11-01 16:03:17 -0400

    Volunteer User Profile UI Component
    Authored by @azhang919

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 // import { UserProvider } from '@auth0/nextjs-auth0';
 import Login from './components/volunteer/Login.jsx';
 import Registration from './components/volunteer/Registration';
-import ProfilePage from './components/volunteer/ProfilePage';
+import Profile from './components/volunteer/Profile';
 import Upload from './components/volunteer/Upload.jsx';
 
 
@@ -11,11 +11,21 @@ function MyApp({ Component, pageProps }) {
 
   return (
     // <UserProvider>
-      <div className="h-screen flex flex-col justify-items-center ">
-        <Upload />
+      <div className="h-screen flex flex-col justify-items-center ">  
+        <Profile />
       </div>
     // </UserProvider>
   )
+
+/*
+return (
+  // <UserProvider>
+    <div className="h-screen flex flex-col justify-items-center ">  
+      <ProfilePage />
+    </div>
+  // </UserProvider>
+)
+*/
 }
 
 export default MyApp
