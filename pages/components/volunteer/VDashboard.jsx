8769db4628107951f01b26bccf3a99186c90cc6d
azhang919 <ayzhang@live.unc.edu>
2022-11-20 08:55:12 -0500

    finished ui

--- pages/components/volunteer/VDashboard.jsx
+++ pages/components/volunteer/VDashboard.jsx
@@ -1,6 +1,47 @@
+import { AiFillClockCircle } from "react-icons/ai";
+import { useState, useRef } from "react";
+
 const VolunteerDashboard = () => {
+  /**
+   * TODO: Fetcha signups data
+   * Filter by user id
+   * Filter by shiftDate from today (inclusive) and onwards
+   */
 
   const Row = (props) => {
+    // Modal display variables
+    const [open, setOpen] = useState(false);
+    const cancelButtonRef = useRef(null);
+
+    /**
+     * TODO: Implement shift cancellation
+     * In the "shifts" table, add 1 to the "remaining_slots" field for this shift
+     * In the "signups" table, delete this record
+     */
+    const cancelShift = async () => {};
+
+    /**
+     * TODO: Implement clocking in
+     * Check that the shift date matches current date
+     * Check that the user is clocking in within 5 min (before or after) shift start time
+     * If not valid, display some kind of alert that user can't clock in right now!
+     *
+     * In "signups" table, update the record with current time as clock-in time
+     *
+     * Update from icon to displaying clock-in time in the date
+     */
+    const clockIn = async () => {};
+
+    /**
+     * TODO: Implement clocking out
+     * Check that the shift date matches current date
+     * If not valid, display some kind of alert that user can't clock out right now!
+     *
+     * In "signups" table, update the record with current time as clock-out time
+     *
+     * Update from icon to displaying clock-out time in the date
+     */
+    const clockOut = async () => {};
 
     return (
       <tr className="border-t bg-white">
@@ -9,26 +50,71 @@ const VolunteerDashboard = () => {
         <td className="py-2 px-6">5:00 PM</td>
         <td className="py-2 px-3">
           <div className="flex justify-center">
-            <button className="indigo-button-sm">V</button>
+            <button className="text-indigo-600 text-lg hover:text-indigo-800">
+              <AiFillClockCircle size={25} />
+            </button>
           </div>
         </td>
         <td className="py-2 px-4">
           <div className="flex justify-center">
-            <button className="indigo-button-sm">V</button>
+            <button className="text-indigo-600 text-lg hover:text-indigo-800">
+              <AiFillClockCircle size={25} />
+            </button>
           </div>
         </td>
         <td className="py-2 px-4">
           <div className="flex justify-center">
-            <button className="red-button-sm">X</button>
+            <button className="red-button-sm" onClick={() => setOpen(true)}>
+              X
+            </button>
           </div>
         </td>
+        {open ? (
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full z-10 items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <form className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pt-5 pb-2 sm:p-8">
+                  <div className="mt-3 pb-2 text-center sm:mt-0 sm:ml-0 sm:text-left border-b">
+                    <h3 className="text-lg font-medium leading-6 text-gray-900">
+                      Cancel Shift
+                    </h3>
+                  </div>
+                  <div>
+                    <p className="text-lg italic mt-2">
+                      Are you sure you want to cancel this shift?
+                    </p>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 flex flex-row-reverse sm:px-6">
+                  <button
+                    type="button"
+                    className="indigo-button-lg"
+                    onClick={(e) => cancelShift(e)}
+                  >
+                    Yes
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 mr-5 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    No
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
       </tr>
     );
   };
 
   return (
     <div
-      className="overflow-hidden bg-white shadow sm:rounded-lg"
+      className="overflow-scroll bg-white shadow sm:rounded-lg"
       style={{ width: "700px" }}
     >
       <div className="px-4 py-5 sm:px-6">
@@ -72,11 +158,10 @@ const VolunteerDashboard = () => {
               </tr>
             </thead>
             <tbody>
-              <Row key={0}/>
-              <Row key={1}/>
-              <Row key={2}/>
-              <Row key={3}/>
-              <Row key={4}/>
+              <Row />
+              <Row />
+              <Row />
+              <Row />
             </tbody>
           </table>
         </div>
