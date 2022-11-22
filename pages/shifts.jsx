8532ecc74a8a3984f5677234989d900f01ac6d55
azhang919 <ayzhang@live.unc.edu>
2022-11-22 13:09:50 -0500

    partial querying for shifts

--- pages/shifts.jsx
+++ pages/shifts.jsx
@@ -1,38 +1,17 @@
-import React, {
-  useEffect,
-  useState,
-  useRef,
-  useContext,
-  useReducer,
-} from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "./supabaseClient";
 import { useRouter } from "next/router";
-import { FiEdit } from "react-icons/fi";
-
 import Link from "next/link";
-import { waitUntilSymbol } from "next/dist/server/web/spec-extension/fetch-event";
 {
   /** 
     /**
      * TODO:
      * Modify so that a Record takes in parameters (in props object)
      */
-  /** 
-  return (
-    <tr className="grid grid-cols-4 gap-4 px-6 mb-1">
-      <td>01/01/2022</td>
-      <td>5:00 pm</td>
-      <td>6:00 pm</td>
-      <td>
-        <button className="indigo-button-sm">Sign Up</button>
-      </td>
-    </tr>
-  );
-  */
   /**
    * TODO:
    * Query database for current available shifts
-   * - Filter based on upcoming shifts (at least one day into the future), and those with remaining_slots > 0
+   * - Filter based on upcoming shifts, and those with remaining_slots > 0
    * - Run separate queries for orientation vs volunteer shift types
    * - If the "orientation" field in the "profiles" table for the volunteer is FALSE,
    *   DON'T query/display for volunteer shifts
@@ -44,53 +23,40 @@ import { waitUntilSymbol } from "next/dist/server/web/spec-extension/fetch-event
    * Ensure that volunteers somehow CAN'T sign up for the same shift multiple times
    */
 }
-var orientationFlag = false;
 
-/**
- * Method for initially fetching shift information upon render from client-side
- * Should change when converting all interactions with DB to API routes
- */
-/**
-  async function testThing() {
-  let today = new Date().toISOString().slice(0, 10);
-  const { data, error } = await supabase
-    .from("shifts")
-    .select("id, shift_type, shift_date, start_time, end_time")
-    .gt("shift_date", today)
-    .gt("remaining_slots", 0)
-    .eq("shift_type", "volunteer");
-  console.log(data);
-  console.log(data[0].end_time);
-  //console.log(error);
-}
-
-testThing();
-*/
 function fetchResource() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [shifts, setShifts] = useState([]);
 
   // Attempt to retrieve current user and get their information from "profiles" table based on ID
   async function getData() {
     try {
       setLoading(true);
 
-      await supabase.auth.getUser().then(async (data, error) => {
+      await supabase.auth.getUser().then(async (data) => {
         if (data) {
           const id = data.data.user.id;
-          const orientation = data.data.user.orientation;
-          orientationFlag = orientation;
-          await supabase
-            .from("profiles")
-            .select()
-            .eq("id", id)
-            .then((profile, err) => {
-              if (profile) {
-                return profile.data[0];
-              }
-            })
-            .then((user) => {
-              setData(user);
+
+          await supabase.from("profiles").select("id", "orientation").eq("id", id).then(async (user) => {
+            if (user) {
+
+              console.log(user);
+              setUser(user);
+
+              const today = new Date().toISOString().slice(0, 10);
+              const shiftType = user.orientation ? 'volunteer' : 'orientation';
+
+              await supabase.from("shifts").select("shift_type, shift_date, start_time, end_time")
+                .eq('shift_type', shiftType).gte("shift_date", today).gt("remaining_slots", 0)
+                .then((query) => {
+                  if (query) {
+                    console.log(query);
+
+                  }
+
+                })
+            }
             });
         }
       });
@@ -104,108 +70,10 @@ function fetchResource() {
     getData();
   }, []);
 
-  return [data, setData, loading, setLoading];
+  return [user, shifts, setShifts, loading, setLoading];
 }
 
-const findShiftType = () => {
-  const router = useRouter();
-
-  // Modal display variables
-  const [open, setOpen] = useState(false);
-
-  const [data, setData, loading, setLoading] = fetchResource();
-
-  // UI for load state
-  if (loading) return <p>Loading...</p>;
-
-  // UI for unauthenticated user
-  if (!data)
-    return (
-      <div>
-        <p>No profile data.</p>
-        <div className="text-primary-color font-medium hover:underline hover:underline-offset-4">
-          <Link href="/login">Click here to sign in or make an account.</Link>
-        </div>
-      </div>
-    );
-
-  const Option = (props) => <option>{props.label}</option>;
-
-  const checkOrientation = async (e) => {
-    e.preventDefault();
-    let success = false;
-
-    const { error } = await supabase
-      .from("profiles")
-      .eq("id", data.id)
-      .then(() => (success = true));
-
-    if (success) {
-      console.log("aha");
-      orientationFlag = data.orientation;
-
-      setOpen(false);
-    } else {
-      console.log(error);
-    }
-  };
-};
-//^^ Re-fetch/render from database for the same user after update
-const queryShifts = async () => {
-  var dataShifts = [
-    {
-      shift_type: " ",
-      shift_date: " ",
-      start_time: " ",
-      end_time: " ",
-    },
-  ];
-
-  if (orientationFlag) {
-    let today = new Date().toISOString().slice(0, 10);
-
-    const { data, error } = await supabase
-      .from("shifts")
-      .select("shift_type, shift_date, start_time, end_time")
-      .gt("shift_date", today)
-      .gt("remaining_slots", 0)
-      .eq("shift_type", "volunteer");
-    console.log("i");
-
-    dataShifts = data;
-  } else {
-    let today = new Date().toISOString().slice(0, 10);
-    console.log("e");
-
-    const { data, error } = await supabase
-      .from("shifts")
-      .select("shift_type, shift_date, start_time, end_time")
-      .gt("shift_date", today)
-      .gt("remaining_slots", 0)
-      .eq("shift_type", "orientation");
-    dataShifts = data;
-  }
-
-  //test here console
-  //console.log(dataShifts[1].end_time);
-  return dataShifts;
-};
-
-/**
-const oShifts = async () => {
-  let today = new Date().toISOString().slice(0, 10);
-  //console.log('o')
-  const {data, error} = await supabase
-    .from("shifts")
-    .select("shift_type, shift_date, start_time, end_time")
-    .gt("shift_date", today)
-    .gt("remaining_slots", 0)
-    .eq("shift_type", "orientation");
-    setDataShifts(data)
-    console.log(data[0].end_time)
-};
-*/
-const Single_shift = (props) => {
+const Record = (props) => {
   {
     return (
       <tr className="grid grid-cols-4 gap-4 px-6 mb-1">
@@ -219,47 +87,26 @@ const Single_shift = (props) => {
     );
   }
 };
-const Record = (props) => {
-  {
+
+const Shifts = () => {
+
+  const [user, shifts, setShifts, loading, setLoading] = fetchResource();
+  // const shiftType = user.orientation ? 'volunteer' : 'orientation';
+
+  // UI for load state
+  if (loading) return <p>Loading...</p>;
+
+  // UI for unauthenticated user
+  if (!shifts) {
     return (
-      <tr className="grid grid-cols-4 gap-4 px-6 mb-1">
-        <td>06/08/2022</td>
-        <td>1:00 PM</td>
-        <td>3:00 PM</td>
-        <td>
-          <button className="indigo-button-sm">Sign Up</button>
-        </td>
-      </tr>
+      <div>
+        <p>No profile data.</p>
+        <div className="text-primary-color font-medium hover:underline hover:underline-offset-4">
+          <Link href="/login">Click here to sign in or make an account.</Link>
+        </div>
+      </div>
     );
   }
-};
-//As soon as this function can pass in a queried value, this code should work (will need to check queries still)
-async function PrintShiftsAsync() {
-  console.log("printshifts");
-
-  const outputTable = await queryShifts();
-  outputTable.sort((a, b) => (a.shift_date > b.shift_date) ? 1 : -1);
-  const out = [];
-  for (let i = 0; i < outputTable.length; i++) {
-    const sD = outputTable[i].shift_date;
-    const st = outputTable[i].start_time;
-    const nd = outputTable[i].end_time;
-
-    out.push(<Single_shift shiftDate={sD} start={st} end={nd} />);
-  }
-  
-  return out;
-}
-function PrintShifts() {
-  const [data, setData] = useState("");
-  useEffect(() => {
-    PrintShiftsAsync().then((out) => setData(out));
-  });
-
-  return data;
-}
-const Shifts = () => {
-  var shiftType = orientationFlag ? "Volunteer Shifts" : "Orientation Shifts";
 
   return (
     <div
@@ -274,7 +121,7 @@ const Shifts = () => {
 
       <div className="border-t border-gray-200 ml-5">
         <h3 className="text-primary-color italic font-bold mt-5">
-          {shiftType}
+          {"shiftType"} shifts
         </h3>
 
         <table className="orientation-shifts mb-4 w-full">
@@ -287,16 +134,12 @@ const Shifts = () => {
             </tr>
           </thead>
           <tbody>
-            <Record className="bg-gray-50" />
-            <Record className="bg-white" />
-            <Record className="bg-gray-50" />
-            <Record className="bg-white" />
-            <Record className="bg-gray-50" />
-            <PrintShifts />
+            
           </tbody>
         </table>
       </div>
     </div>
   );
 };
+
 export default Shifts;
