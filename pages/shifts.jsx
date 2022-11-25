63ceef9711996e79ad7c4fe4ab1fa5e8e26a9a1d
azhang919 <ayzhang@live.unc.edu>
2022-11-25 15:26:01 -0500

    adjusted UI for loading on initial render

--- pages/shifts.jsx
+++ pages/shifts.jsx
@@ -3,15 +3,6 @@ import { supabase } from "./supabaseClient";
 import Link from "next/link";
 import Navbar from "./navbar";
 
-/**
- * TODO:
- * IF VOLUNTEER HAS ALREADY SIGNED UP FOR AN ORIENTATION SHIFT, DON'T DISPLAY ANYMORE
- *
- * If no shifts are queried or returned in any case, create blank UI
- * - "No available shifts at the moment!"
- * - "You're already signed up for an orientation shift! View upcoming shifts in your dashboard."
- */
-
 const Shifts = () => {
   // Modal variables
   const [open, setOpen] = useState(false);
@@ -47,7 +38,7 @@ const Shifts = () => {
 
                 setUser(res.data[0]);
                 setShiftType(sType.charAt(0).toUpperCase() + sType.slice(1));
-                
+
                 // Query shift information
                 fetchShifts(uid, today, sType);
                 success = true;
@@ -63,14 +54,14 @@ const Shifts = () => {
     }
   }
 
-  /** 
+  /**
    * Attempts to retrieve:
    * upcoming shifts from current date and onwards,
    * of the necessary shift type (either orientation or volunteer),
    * with open slots left,
    * that the user has not already signed up for,
    * sorted by shift date and start time
-  */
+   */
   async function fetchShifts(uid, currentDate, shiftType) {
     await supabase
       .from("signups")
@@ -106,7 +97,7 @@ const Shifts = () => {
   useEffect(() => {
     getData().then(() => {
       setLoading(false);
-    })
+    });
 
     const subscription = supabase
       .channel("shift-changes")
@@ -124,13 +115,16 @@ const Shifts = () => {
     };
   }, []);
 
-
   // Empty UI for Loading State
   if (loading) {
     return (
-      <div className="flex w-screen h-screen justify-center items-center">
+      <div className="flex flex-row">
+        <Navbar />
+        <div className="container p-10">
+          <div className="shadow sm:rounded-lg w-4/5"></div>
+        </div>
       </div>
-    )
+    );
   }
 
   // UI for unauthenticated user
@@ -166,7 +160,7 @@ const Shifts = () => {
   const volunteer = async (e, s) => {
     e.preventDefault();
     let success = false;
-    
+
     try {
       await supabase
         .from("shifts")
@@ -212,83 +206,89 @@ const Shifts = () => {
     <div className="flex flex-row">
       <Navbar />
       <div className="container p-10">
-    <div className="shadow sm:rounded-lg w-4/5">
-      <div className="px-4 py-5 sm:px-6">
-          <h2 className="text-lg font-medium leading-6 text-gray-900">
-            Available {shiftType} Shifts
-          </h2>
-        </div>
+        <div className="shadow sm:rounded-lg w-4/5">
+          <div className="px-4 py-5 sm:px-6">
+            <h2 className="text-lg font-medium leading-6 text-gray-900">
+              Available {shiftType} Shifts
+            </h2>
+          </div>
 
-        <div className="border-t border-gray-200 ml-5">
-          <table className="orientation-shifts mt-5 mb-4 w-full">
-            <thead>
-              <tr className="grid grid-cols-4 gap-4 px-6 text-left">
-                <th className="font-medium">Date</th>
-                <th className="font-medium">Start Time</th>
-                <th className="font-medium">End Time</th>
-                <th className="font-medium"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {shifts !== [] ? (shifts.map((s) => {
-                return (
-                  <>
-                    <tr key={s.id} className="grid grid-cols-4 gap-4 px-6 mb-1">
-                      <td>{formatDate(s.shift_date)}</td>
-                      <td>{formatTime(s.start_time)}</td>
-                      <td>{formatTime(s.end_time)}</td>
-                      <td>
-                        <button
-                          className="indigo-button-sm"
-                          onClick={(e) => volunteer(e, s)}
+          <div className="border-t border-gray-200 ml-5">
+            {false ? (
+              <table className="orientation-shifts mt-5 mb-4 w-full">
+                <thead>
+                  <tr className="grid grid-cols-4 gap-4 px-6 text-left">
+                    <th className="font-medium">Date</th>
+                    <th className="font-medium">Start Time</th>
+                    <th className="font-medium">End Time</th>
+                    <th className="font-medium"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {shifts.map((s) => {
+                    return (
+                      <>
+                        <tr
+                          key={s.id}
+                          className="grid grid-cols-4 gap-4 px-6 mb-1"
                         >
-                          Sign Up
-                        </button>
-                        {open ? (
-                          <div className="fixed inset-0 z-10 overflow-y-auto">
-                            <div className="flex min-h-full z-10 items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                              <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                                <div className="bg-white px-4 pt-5 pb-4 sm:p-8">
-                                  <div className="mt-3 text-center sm:mt-0 sm:ml-0 sm:text-left">
-                                    <h3 className="text-lg font-medium leading-6 text-gray-900">
-                                      Status
-                                    </h3>
-                                    <hr />
-                                    <div className="mt-2">
-                                      <p className="text-sm text-gray-500">
-                                        {message}
-                                      </p>
+                          <td>{formatDate(s.shift_date)}</td>
+                          <td>{formatTime(s.start_time)}</td>
+                          <td>{formatTime(s.end_time)}</td>
+                          <td>
+                            <button
+                              className="indigo-button-sm"
+                              onClick={(e) => volunteer(e, s)}
+                            >
+                              Sign Up
+                            </button>
+                            {open ? (
+                              <div className="fixed inset-0 z-10 overflow-y-auto">
+                                <div className="flex min-h-full z-10 items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                                  <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                                    <div className="bg-white px-4 pt-5 pb-4 sm:p-8">
+                                      <div className="mt-3 text-center sm:mt-0 sm:ml-0 sm:text-left">
+                                        <h3 className="text-lg font-medium leading-6 text-gray-900">
+                                          Status
+                                        </h3>
+                                        <hr />
+                                        <div className="mt-2">
+                                          <p className="text-sm text-gray-500">
+                                            {message}
+                                          </p>
+                                        </div>
+                                      </div>
+                                    </div>
+                                    <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                      <button
+                                        type="button"
+                                        className="indigo-button-lg"
+                                        onClick={() => setOpen(false)}
+                                      >
+                                        OK
+                                      </button>
                                     </div>
                                   </div>
                                 </div>
-                                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                  <button
-                                    type="button"
-                                    className="indigo-button-lg"
-                                    onClick={() => setOpen(false)}
-                                  >
-                                    OK
-                                  </button>
-                                </div>
                               </div>
-                            </div>
-                          </div>
-                        ) : (
-                          <div></div>
-                        )}
-                      </td>
-                    </tr>
-                  </>
-                );
-              })) : (
-                <tr>
-                  No available shifts at the moment! Check back in later.
-                </tr>
-              )}
-            </tbody>
-          </table>
+                            ) : (
+                              <div></div>
+                            )}
+                          </td>
+                        </tr>
+                      </>
+                    );
+                  })}
+                  ;
+                </tbody>
+              </table>
+            ) : (
+              <div className="mt-5 w-full">
+                No available shifts at the moment! Check back in later.
+              </div>
+            )}
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
