c4324caa0362ac1bd8edabda305cb852679a3b96
Alison Zhang <59739942+azhang919@users.noreply.github.com>
2022-11-27 18:58:02 -0500

    Fixed small UI styling and redundant code (#23)

--- pages/components/navbar.jsx
+++ pages/components/navbar.jsx
@@ -7,46 +7,28 @@ const Navbar = () => {
 
   const logout = async () => {
     let success = false;
-    await supabase.auth.signOut().then(() => success = true);
+    await supabase.auth.signOut().then(() => {
+      success = true;
+    });
     if (success) router.push("/login");
   };
 
   return (
     <div className="w-40 h-screen shadow-md bg-primary-color text-white sm:text-sm px-1">
       <ul className="relative mt-6 space-y-4 pl-6">
-        <li
-          className="relative overflow-hidden text-white text-ellipsis whitespace-nowrap rounded hover:text-bg-colortransition duration-300 ease-in-out"
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="dark"
-        >
+        <li className="navbar-item">
           <Link href="/profile">Profile</Link>
         </li>
-        <li
-          className="relative overflow-hidden text-white text-ellipsis whitespace-nowrap rounded hover:text-bg-colortransition duration-300 ease-in-out"
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="dark"
-        >
+        <li className="navbar-item">
           <Link href="/dashboard">Dashboard</Link>
         </li>
-        <li
-          className="relative overflow-hidden text-white text-ellipsis whitespace-nowrap rounded hover:text-bg-colortransition duration-300 ease-in-out"
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="dark"
-        >
+        <li className="navbar-item">
           <Link href="/shifts">Shifts</Link>
         </li>
-        <li
-          className="relative overflow-hidden text-white text-ellipsis whitespace-nowrap rounded hover:text-bg-colortransition duration-300 ease-in-out"
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="dark"
-        >
+        <li className="navbar-item">
           <Link href="/files">Files</Link>
         </li>
-        <li
-          className="relative overflow-hidden text-white text-ellipsis whitespace-nowrap rounded hover:text-bg-colortransition duration-300 ease-in-out"
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="dark"
-        >
+        <li className="navbar-item">
           <button onClick={logout}>Logout</button>
         </li>
       </ul>
--- pages/shifts.jsx
+++ pages/shifts.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import Navbar from "./components/navbar";
-import Loading from './components/loading';
-import Rerouting from './components/rerouting';
+import Loading from "./components/loading";
+import Rerouting from "./components/rerouting";
 import { supabase } from "./supabaseClient";
+import { formatTime, formatDate } from "./components/formatting";
 
 const Shifts = () => {
   // Modal variables
@@ -118,26 +119,13 @@ const Shifts = () => {
 
   // Empty UI for Loading State
   if (loading) {
-    return <Loading/>
+    return <Loading />;
   }
 
   // UI for unauthenticated user
   if (!user) {
-    return <Rerouting/>
+    return <Rerouting />;
   }
-
-  // Format from military time to 12-hour with "AM" or "PM" specifications
-  const formatTime = (time) => {
-    let hour = parseInt(time.slice(0, 2));
-    let suffix = hour > 12 ? "PM" : "AM";
-    hour = String(hour > 12 ? hour - 12 : hour).padStart(2, "0");
-    return hour + ":" + time.slice(3, 5) + " " + suffix;
-  };
-
-  // Format from YYYY-MM-DD to MM/DD/YYYY
-  const formatDate = (date) => {
-    return date.slice(5, 7) + "/" + date.slice(8, 10) + "/" + date.slice(0, 4);
-  };
 
   /**
    * Attempts to volunteer user for specific shift
@@ -193,7 +181,7 @@ const Shifts = () => {
       <div className="container p-10">
         <div className="shadow sm:rounded-lg w-4/5">
           <div className="px-4 py-5 sm:px-6">
-            <h2 className="text-lg font-medium leading-6 text-gray-900">
+            <h2>
               Available {shiftType} Shifts
             </h2>
           </div>
@@ -212,56 +200,22 @@ const Shifts = () => {
                 <tbody>
                   {shifts.map((s) => {
                     return (
-                      <>
-                        <tr
-                          key={s.id}
-                          className="grid grid-cols-4 gap-4 px-6 mb-1"
-                        >
-                          <td>{formatDate(s.shift_date)}</td>
-                          <td>{formatTime(s.start_time)}</td>
-                          <td>{formatTime(s.end_time)}</td>
-                          <td>
-                            <button
-                              className="'-sm"
-                              onClick={(e) => volunteer(e, s)}
-                            >
-                              Sign Up
-                            </button>
-                            {open ? (
-                              <div className="fixed inset-0 z-10 overflow-y-auto">
-                                <div className="flex min-h-full z-10 items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                                  <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                                    <div className="bg-white px-4 pt-5 pb-4 sm:p-8">
-                                      <div className="mt-3 text-center sm:mt-0 sm:ml-0 sm:text-left">
-                                        <h3 className="text-lg font-medium leading-6 text-gray-900">
-                                          Status
-                                        </h3>
-                                        <hr />
-                                        <div className="mt-2">
-                                          <p className="text-sm text-gray-500">
-                                            {message}
-                                          </p>
-                                        </div>
-                                      </div>
-                                    </div>
-                                    <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                      <button
-                                        type="button"
-                                        className="indigo-button-lg"
-                                        onClick={() => setOpen(false)}
-                                      >
-                                        OK
-                                      </button>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            ) : (
-                              <div></div>
-                            )}
-                          </td>
-                        </tr>
-                      </>
+                      <tr
+                        key={s.id}
+                        className="grid grid-cols-4 gap-4 px-6 mb-1"
+                      >
+                        <td>{formatDate(s.shift_date)}</td>
+                        <td>{formatTime(s.start_time)}</td>
+                        <td>{formatTime(s.end_time)}</td>
+                        <td>
+                          <button
+                            className="indigo-button-sm"
+                            onClick={(e) => volunteer(e, s)}
+                          >
+                            Sign Up
+                          </button>
+                        </td>
+                      </tr>
                     );
                   })}
                 </tbody>
@@ -272,6 +226,37 @@ const Shifts = () => {
               </div>
             )}
           </div>
+
+          {open ? (
+            <div className="fixed inset-0 z-10 overflow-y-auto">
+              <div className="flex min-h-full z-10 items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <div className="bg-white px-4 pt-5 pb-4 sm:p-8">
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-0 sm:text-left">
+                      <h3 className="text-lg font-medium leading-6 text-gray-900">
+                        Status
+                      </h3>
+                      <hr />
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">{message}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button
+                      type="button"
+                      className="indigo-button-lg"
+                      onClick={() => setOpen(false)}
+                    >
+                      OK
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div></div>
+          )}
         </div>
       </div>
     </div>
